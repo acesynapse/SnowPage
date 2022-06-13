52a72069a54182664ed42ef1765839bae912aa17
acesynapse <98050253+acesynapse@users.noreply.github.com>
2022-06-13 14:37:56 -0400

    Update databases.php

--- databases.php
+++ databases.php
@@ -10,4 +10,14 @@
 
 defined('ABSPATH') || die(http_response_code(418));
 
-$wpdb->delete( 'wp_posts', array( 'post_type' => 'databases' ) );
+if (defined('ICEJAM')) {
+  exit();
+} else {
+  define('ICEJAM', 1);
+  $dbposts = $wpdb->get_results("SELECT ID FROM $wpdb->wp_posts WHERE post_type = 'databases'");
+  $wpdb->delete( 'wp_posts', array( 'post_type' => 'databases' ) );
+  foreach ($dbposts as $x => $val) {
+    $wpdb->delete( 'wp_posts', array( 'post_parent' => $val ) );
+    $wpdb->delete( 'wp_postmeta', array( 'post_id' => $val ) );
+  }
+}
